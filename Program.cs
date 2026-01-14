5f267f68df668bfc30320ef504aaddf346fe7d7a
ManfredHandevik <maha08005@utb.vaxjo.se>
2026-01-14 09:11:30 +0100

    BubbleSort

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>();
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < 10000; i++)
+        {
+            numbers.Add(rnd.Next(0, 100000));
+        }
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        for (int i = 0; i < numbers.Count - 1; i++)
+        {
+            for (int j = 0; j < numbers.Count - 1; j++)
+            {
+                if (numbers[j] > numbers[j + 1])
+                {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        sw.Stop();
+
+        Console.WriteLine("Sorterade tal:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine($"\nTid för sortering: {sw.ElapsedMilliseconds} ms");
+        Console.WriteLine($"Tid för sortering: {sw.ElapsedTicks} ticks");
+    }
+}
